febd1169ee50a44cc38dd7d4a52c2879edda974d
Jim Rollenhagen <jim@jimrollenhagen.com>
2017-07-18 15:37:38 -0400

    add graphql code

---
+++ flask_graphql_views/views.py
@@ -0,0 +1,159 @@
+"""
+Intends to provide conveniences around mounting a Graphene schema
+
+"""
+import json
+
+from flask import request, Response
+from flask.views import MethodView
+
+from flask_graphql_views import graphiql
+
+
+def format_error(error):
+    """
+    Taken from graphql-python/graphql-core, modified to be a little more
+    robust.
+    https://github.com/graphql-python/graphql-core/blob/master/graphql/error/format_error.py
+
+    """
+    formatted_error = {
+        'message': error.message if hasattr(error, 'message') else str(error)
+    }
+
+    if hasattr(error, 'locations') and error.locations is not None:
+        formatted_error['locations'] = [
+            {'line': loc.line, 'column': loc.column}
+            for loc in error.locations
+        ]
+
+    return formatted_error
+
+
+class GraphQLResult(object):
+    def __init__(self, result):
+        self.result = result
+
+    def to_response(self):
+        return Response(json.dumps(self.result),
+                        status=200,
+                        mimetype='application/json')
+
+
+class GraphQLView(MethodView):
+    def __init__(self, *args, sentry=None, result_class=None, schema=None,
+                 enable_graphiql=True):
+        super().__init__()
+        self.schema = schema
+        self.enable_graphiql = enable_graphiql
+        self.sentry = sentry.client if sentry else None
+        self.result_class = result_class or GraphQLResult
+
+    @property
+    def can_display_graphiql(self):
+        """Determines if we are *allowed* to display the UI"""
+        return self.enable_graphiql or 'raw' not in request.args
+
+    @property
+    def request_wants_html(self):
+        """
+        Determines if the inbound request desires an HTTP response.
+        Taken from flask_graphql, refactor.
+
+        """
+        best = request.accept_mimetypes.best_match(
+            ['application/json', 'text/html'])
+
+        return (best == 'text/html' and
+                request.accept_mimetypes[best] >
+                request.accept_mimetypes['application/json'])
+
+    @property
+    def should_display_graphiql(self):
+        """Determines if we *should* show the graphiql UI."""
+        return self.can_display_graphiql and self.request_wants_html
+
+    @property
+    def params(self):
+        request_keys = {'query', 'operationName', 'variables'}
+
+        if request.method == 'GET':
+            # we will collect the data from the query parameters,
+            # such as ?query={}
+            return {k: request.args.get(k) for k in request_keys}
+
+        if request.method == 'POST':
+            if request.mimetype == 'application/graphql':
+                return {'query': request.data.decode('utf8')}
+
+            elif request.mimetype == 'application/json':
+                return request.json
+
+            elif request.mimetype in ('application/x-www-form-urlencoded',
+                                      'multipart/form-data'):
+                return request.form
+
+        # Else, return an empty map
+        return {k: None for k in request_keys}
+
+    def format_execution_result(self, result):
+        """
+        Transforms the graphql.execution.base.ExecutionResult into a JSON
+        encodable entity.
+
+        """
+        if not result:
+            return None
+
+        resp = {}
+
+        if result.data:
+            resp['data'] = result.data
+
+        if result.errors:
+            resp['errors'] = [format_error(e) for e in result.errors]
+
+        return resp
+
+    def track_errors(self, result):
+        if not self.sentry:
+            return
+
+        for e in result.errors:
+            self.sentry.captureMessage(e, extra={
+                'params': self.params
+            })
+
+    def handle_request(self):
+        params = self.params
+
+        # This is where we actually submit our query to the graphql schema
+        if params.get('query'):
+            result = self.schema.execute(
+                params.get('query'), variable_values=params.get('variables'))
+        else:
+            result = None
+
+        # this is where we would capture graphql errors and send to sentry
+        if result and result.errors:
+            self.track_errors(result)
+
+        return self.format_execution_result(result)
+
+    def get(self):
+        """
+        For displaying the graphiql client, and for query requests
+
+        """
+        result = self.handle_request()
+
+        if self.should_display_graphiql:
+            return graphiql.render(params=self.params, result=result)
+
+        return self.result_class(result)
+
+    def post(self):
+        """For mutations"""
+        result = self.handle_request()
+
+        return self.result_class(result)
