9c358bd13520da7dcdd05be9b2a941097201ca99
Jim Rollenhagen <jim@jimrollenhagen.com>
2017-07-19 15:50:13 -0400

    add first round of unit tests

---
+++ tests/test_views.py
@@ -0,0 +1,53 @@
+from flasql import views
+
+
+class MockLocation(object):
+    def __init__(self, line, column):
+        self.line = line
+        self.column = column
+
+
+class MockError(object):
+    def __init__(self, message=None, locations=None):
+        if message is not None:
+            self.message = message
+        if locations is not None:
+            self.locations = locations
+
+    def __str__(self):
+        return 'string representation'
+
+
+def test_format_error_no_details():
+    e = MockError()
+    formatted = views.format_error(e)
+    assert formatted['message'] == 'string representation'
+    assert 'locations' not in formatted
+
+
+def test_format_error_with_message():
+    e = MockError(message='bailed out')
+    formatted = views.format_error(e)
+    assert formatted['message'] == 'bailed out'
+    assert 'locations' not in formatted
+
+
+def test_format_error_with_one_location():
+    locations = [MockLocation(3, 42)]
+    e = MockError(message='bailed out', locations=locations)
+    formatted = views.format_error(e)
+    assert formatted['message'] == 'bailed out'
+    assert len(formatted['locations']) == 1
+    assert formatted['locations'] == [{'line': 3, 'column': 42}]
+
+
+def test_format_error_with_two_locations():
+    locations = [MockLocation(3, 42), MockLocation(7, 57)]
+    e = MockError(message='bailed out', locations=locations)
+    formatted = views.format_error(e)
+    assert formatted['message'] == 'bailed out'
+    assert len(formatted['locations']) == 2
+    assert formatted['locations'] == [
+        {'line': 3, 'column': 42},
+        {'line': 7, 'column': 57},
+    ]
