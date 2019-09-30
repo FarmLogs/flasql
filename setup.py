a0962b055fbc7ad33b866d3873b6b6103ae9302c
Brad Koch <brad@farmlogs.com>
2019-09-30 14:23:10 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='flasql',
-    version='0.0.1',
+    version='0.0.2',
     description='Basic flask views to provide graphql endpoints',
     author='FarmLogs',
     packages=['flasql'],
